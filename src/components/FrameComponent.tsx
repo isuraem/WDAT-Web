3dd0dd19dcb0334d65396267a390fc0525b6188c
isuraem <erandaim@gmail.com>
2024-04-03 18:30:19 +0530

    removed_unbwanted_codes

--- src/components/FrameComponent.tsx
+++ src/components/FrameComponent.tsx
@@ -1,38 +1,3 @@
-// import { FunctionComponent } from "react";
-// import Todoitem from "./TodoItem";
-// import "./FrameComponent.css";
-
-// interface AppProps {
-//     todos: ITodo[]; // Define the todos prop
-// }
-
-// const FrameComponent: React.FC<AppProps> = ({ todos }) => {
-//     return (
-//         <div className="tasks-to-do-4-parent">
-//             <div className="tasks-to-do">Tasks to do - 4</div>
-//             <Todoitem />
-//             <Todoitem check="/check-1.svg" />
-//             <div className="todoitem2">
-//                 <div className="todoitem-inner" />
-//                 <input
-//                     className="frame-input"
-//                     placeholder="To study React fundamentals"
-//                     type="text"
-//                 />
-//                 <div className="frame-group">
-//                     <div className="rectangle-group">
-//                         <div className="frame-item" />
-//                         <img className="check-icon1" alt="" src="/check-2.svg" />
-//                     </div>
-//                     <input className="group-input1" type="checkbox" />
-//                 </div>
-//             </div>
-//             <Todoitem check="/check-3.svg" />
-//         </div>
-//     );
-// };
-
-// export default FrameComponent;
 import React from "react";
 import Todoitem from "./TodoItem";
 import "./FrameComponent.css";
